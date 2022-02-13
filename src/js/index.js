07c4b28c430eea79556e18b4c71d9c33b74b839b
‘свой <ebertzz90@gmail.com>
2022-02-13 22:54:54 +0300

    added popup-window

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import '../styles/index.scss';
 import '../styles/fonts.scss';
 import '../styles/mixin.scss';
+import './popup';
 import './component';
 import './slider';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
 
 // const app = document.getElementById('app');
 // const greeting = 'Hello World';
